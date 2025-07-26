a85e4dbcfac034dbd7789012e3b2800f5b76e877
kadzx <954003280@qq.com>
2025-07-26 17:41:14 +0800

    用户注册功能，轻提示函数

--- js/common.js
+++ js/common.js
@@ -1,1 +1,8 @@
 axios.defaults.baseURL='https://hmajax.itheima.net';
+
+function showToast(msg){
+    const toastDom=document.querySelector('.my-toast');
+    const toast=new bootstrap.Toast(toastDom);
+    document.querySelector('.toast-body').innerHTML=msg
+    toast.show();
+}
