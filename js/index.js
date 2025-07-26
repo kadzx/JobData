0e33672b412d3868d98d75940d44bedd652bc033
kadzx <954003280@qq.com>
2025-07-26 18:48:42 +0800

    获取学生数据

--- js/index.js
+++ js/index.js
@@ -2,3 +2,20 @@ const loginObj=checkToken();
 renderUsername(loginObj);
 //判断是否登录并且同步用户信息
 registerLogout();
+
+const token=loginObj.token;
+//获取学生统计数据
+async function getData() {
+    const res= await axios({
+        url: '/dashboard',
+        headers:{
+            Authorization:token
+        }
+    })
+    console.log(res)
+    const overview=res.data.data.overview;
+    Object.keys(overview).forEach(key=>{
+        document.querySelector(`.${key}`).innerHTML=overview[key]
+    }) 
+}
+getData()
